37c3be6097a0358aee024b1671323166e611d90c
amoras1601 <gustavoamorim.matos1@gmail.com>
2026-03-03 19:49:03 -0300

    add tela de login/tela de pedidos, edição do header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'; // O React entende que é pra pegar o index.jsx dentro da pasta Home
 import { Cart } from './pages/cart/index.jsx'
 import { Profile } from './pages/Client/Profile.jsx'
+import { Login } from './pages/Login/index.jsx'
+import { Orders } from './pages/Orders/Orders.jsx';
 
 export default function App() {
   return (
@@ -11,7 +13,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/perfil" element={<Profile />} />
-        {/* Futuramente: <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/pedidos" element={<Orders />} />
       </Routes>
     </BrowserRouter>
   );
